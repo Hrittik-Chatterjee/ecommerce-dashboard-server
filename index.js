528ec607e7f1f3b052dc0e9017758743160251ba
Hrittik-Chatterjee <hrittik0chatterjee@gmail.com>
2024-09-15 21:34:13 +0600

    removed body parser

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const bodyParser = require("body-parser"); // Needed to process Stripe webhooks
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb"); // Needed to process Stripe webhooks
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET; // Add webhook secret
 
 console.log(endpointSecret);
 
 app.use(cors());
+app.use(express.json());
 
-app.use(bodyParser.raw({ type: "application/json" })); // For Stripe webhook handling
+// For Stripe webhook handling
 
 const port = process.env.PORT || 5000;
 
@@ -181,11 +181,11 @@ async function run() {
     // Stripe webhook to handle payment success
     app.post(
       "/webhook",
-      bodyParser.raw({ type: "application/json" }),
+      express.raw({ type: "application/json" }), // Use express.raw() instead of bodyParser.raw()
       async (req, res) => {
         const sig = req.headers["stripe-signature"];
-
         let event;
+
         try {
           event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
         } catch (err) {
@@ -219,7 +219,7 @@ async function run() {
     // Close the connection when done
   }
 }
-app.use(express.json());
+
 run().catch(console.log);
 
 // Test route
